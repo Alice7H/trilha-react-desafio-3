a53b4dabf9cd4ced275601b6bb81241dfaa8a5c0
Alice Hata <alicehata9@gmail.com>
2022-11-24 22:16:44 -0300

    redirect to login and user registration

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/logo-dio.png';
-
 import { Button } from '../Button';
-
 import { Container, Wrapper, BuscarInputContainer, Input, Row, Menu, MenuRight, UserPicture} from './styles';
 
 const Header = ({autenticado}) => {
+  const navigate = useNavigate();
+  const handleLoginAccount = () => { navigate('/login') };
+  const handleRegisterAccount = () => { navigate('/register') };
+
   return (
     <Wrapper>
       <Container>
@@ -27,8 +29,8 @@ const Header = ({autenticado}) => {
               ) : (
               <>
                 <MenuRight href="/">Home</MenuRight>
-                <Button title="Entrar" />
-                <Button title="Cadastrar" />
+                <Button onClick={handleLoginAccount} title="Entrar" />
+                <Button onClick={handleRegisterAccount} title="Cadastrar" />
               </>)}
           </Row>
       </Container>
